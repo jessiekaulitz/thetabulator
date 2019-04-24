2b9097fb37d3a23e1dee518738c8167520bbcb3d
John Seymour <john.seymour787@gmail.com>
2019-04-24 13:25:19 +1000

    Add basic calendar component and classes

---
+++ TheTabulator/TheTabulator/Event.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TheTabulator
+{
+    public class Event
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private int _color;
+        private string _name;
+        private string _location;
+
+        public Event(string name)
+        {
+            _name = name;
+        }
+
+        public void SaveEvent(StreamWriter streamWriter)
+        {
+
+        }
+
+        public void LoadEvent(StreamReader streamReader)
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+        public string Location
+        {
+            get
+            {
+                return _location;
+            }
+
+            set
+            {
+                _location = value;
+            }
+        }
+
+        public int Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            set
+            {
+                _color = value;
+            }
+        }
+
+        public DateTime StartTime
+        {
+            get
+            {
+                return _startTime;
+            }
+
+            set
+            {
+                _startTime = value;
+            }
+        }
+
+        public DateTime EndTime
+        {
+            get
+            {
+                return _endTime;
+            }
+
+            set
+            {
+                _endTime = value;
+            }
+        }
+    }
+}
---
+++ TheTabulator/TheTabulator/RecurringEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTabulator
+{
+    class RecurringEvent: Event
+    {
+        private Event _nextEvent;
+
+        public RecurringEvent(string name) : base (name)
+        {
+
+        }
+    }
+}
