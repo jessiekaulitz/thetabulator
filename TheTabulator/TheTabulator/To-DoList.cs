b95a87058020589cbab70b0c8081d092e08b4ca1
Natalie <nataliesholakis@gmail.com>
2019-05-01 18:04:05 +1000

    Iteration 2

--- TheTabulator/TheTabulator/To-DoList.cs
+++ TheTabulator/TheTabulator/To-DoList.cs
@@ -19,29 +19,75 @@ namespace TheTabulator
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pnlMainToDoList = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListView();
+            this.clmhToDoListName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
             this.txtTaskDescription = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.txtToDoListName = new System.Windows.Forms.TextBox();
-            this.lstTasks = new System.Windows.Forms.ListView();
             this.pnlMainToDoList.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlMainToDoList
             // 
+            this.pnlMainToDoList.Controls.Add(this.button1);
             this.pnlMainToDoList.Controls.Add(this.lstTasks);
             this.pnlMainToDoList.Controls.Add(this.txtTaskDescription);
             this.pnlMainToDoList.Controls.Add(this.btnAdd);
             this.pnlMainToDoList.Controls.Add(this.txtToDoListName);
             this.pnlMainToDoList.Location = new System.Drawing.Point(0, 0);
             this.pnlMainToDoList.Name = "pnlMainToDoList";
-            this.pnlMainToDoList.Size = new System.Drawing.Size(435, 781);
+            this.pnlMainToDoList.Size = new System.Drawing.Size(525, 683);
             this.pnlMainToDoList.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button1.Location = new System.Drawing.Point(436, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 70);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add Step";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.AllowColumnReorder = true;
+            this.lstTasks.CheckBoxes = true;
+            this.lstTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmhToDoListName});
+            this.lstTasks.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lstTasks.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lstTasks.LabelEdit = true;
+            this.lstTasks.LabelWrap = false;
+            this.lstTasks.Location = new System.Drawing.Point(4, 123);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(428, 558);
+            this.lstTasks.SmallImageList = this.imageListSmall;
+            this.lstTasks.TabIndex = 3;
+            this.lstTasks.UseCompatibleStateImageBehavior = false;
+            this.lstTasks.View = System.Windows.Forms.View.Details;
+            this.lstTasks.SelectedIndexChanged += new System.EventHandler(this.LstTasks_SelectedIndexChanged);
+            // 
+            // clmhToDoListName
+            // 
+            this.clmhToDoListName.Text = "";
+            this.clmhToDoListName.Width = 428;
+            // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageListSmall.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // txtTaskDescription
             // 
             this.txtTaskDescription.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTaskDescription.Location = new System.Drawing.Point(4, 71);
+            this.txtTaskDescription.Location = new System.Drawing.Point(4, 66);
             this.txtTaskDescription.Name = "txtTaskDescription";
             this.txtTaskDescription.Size = new System.Drawing.Size(359, 41);
             this.txtTaskDescription.TabIndex = 2;
@@ -53,9 +99,9 @@ namespace TheTabulator
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Tahoma", 14F);
-            this.btnAdd.Location = new System.Drawing.Point(369, 66);
+            this.btnAdd.Location = new System.Drawing.Point(373, 65);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(63, 51);
+            this.btnAdd.Size = new System.Drawing.Size(63, 50);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "+";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -71,24 +117,11 @@ namespace TheTabulator
             this.txtToDoListName.Text = "Untitled";
             this.txtToDoListName.TextChanged += new System.EventHandler(this.TxtToDoListName_TextChanged);
             // 
-            // lstTasks
-            // 
-            this.lstTasks.CheckBoxes = true;
-            this.lstTasks.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.lstTasks.LabelEdit = true;
-            this.lstTasks.LabelWrap = false;
-            this.lstTasks.Location = new System.Drawing.Point(4, 119);
-            this.lstTasks.Name = "lstTasks";
-            this.lstTasks.Size = new System.Drawing.Size(428, 659);
-            this.lstTasks.TabIndex = 3;
-            this.lstTasks.UseCompatibleStateImageBehavior = false;
-            this.lstTasks.View = System.Windows.Forms.View.List;
-            // 
             // To_DoList
             // 
             this.Controls.Add(this.pnlMainToDoList);
             this.Name = "To_DoList";
-            this.Size = new System.Drawing.Size(438, 781);
+            this.Size = new System.Drawing.Size(525, 683);
             this.Load += new System.EventHandler(this.To_DoList_Load);
             this.pnlMainToDoList.ResumeLayout(false);
             this.pnlMainToDoList.PerformLayout();
@@ -157,5 +190,28 @@ namespace TheTabulator
         }
 
         private ListView lstTasks;
+        private ColumnHeader clmhToDoListName;
+
+        private void LstTasks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        private Button button1;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem task in lstTasks.Items)
+            {
+                if (task.Selected)
+                {
+                    lstTasks.Items.Insert(task.Index + 1, "New Step");
+                    lstTasks.Items[task.Index + 1].IndentCount = task.IndentCount + 1;
+                }
+            }
+        }
+
+        private ImageList imageListSmall;
+        private IContainer components;
     }
 }
